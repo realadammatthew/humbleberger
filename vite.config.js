dfe3fb3133925f6f5f02e3418d798e0382d5a574
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-03 21:41:38 -0400

    switched to ghost blog

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7316d6c1bea4c6868626509c50005ee898db289bb5c34c1f44377233eae1436b
-size 592
+oid sha256:7fbf05c6143775234540c7ce88b928b60b847f2035b5b53fca6e98f5b13ee1ba
+size 474
