b091aa26910e27134d4a37de6e10639e513751e4
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-20 11:12:35 -0400

    added reference to Jeremiah 20:9

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a6c689a948e95a991bfaf5d32ba380d5f07036e8d9721932a50d03befebd006e
-size 1613
+oid sha256:6c6f5ae24e5dda0a55bdb288cb237031b07437c616949bd82086c3d258c3c51a
+size 1840
