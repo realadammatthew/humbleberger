882143e1b7b049440ac3df4c0491e15d7af562e0
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-21 11:06:26 -0400

    hoping this fixed the contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:83b8394b94c0310c1cea41fe2fa43eeeeafef93acfd2f7224380055f85366eac
-size 3615
+oid sha256:958ab3c0e8e3ace902f5f2ee9143bec5bcfb96652a089f51bc34644c3305512b
+size 4956
