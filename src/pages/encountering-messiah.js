a0dfd601a7a148d553dbd47582e337a20958c962
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-21 17:01:49 -0400

    added new encountering messiah landing page

---
+++ src/pages/encountering-messiah.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c21f037735102d63c3f6136eaa6f548bca0fa91a94445e647f06646483775c8d
+size 7074
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:917dee5c0f0cf392cf1dcb81d4ee03c03b23fc422b994e6968e6943eafa984d8
-size 4694
+oid sha256:5c7eae82580e12a02928cc4eb36872182decdeb660501f05609f75cd237f77da
+size 5399
