570c3b2348e4351a3efbf12b36b783c376a7b725
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-21 16:00:59 -0400

    blog posts list now looks more like a table of contents like it should!

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6415d9b151dba6a2f19b0349b5beb9a443bd5450d15acce8ef500c6fe4adcc9d
-size 2478
+oid sha256:feb898ac50721fcb92d309b31999665dfe314a10f1057a0304b62c097e0d7b0a
+size 2349
