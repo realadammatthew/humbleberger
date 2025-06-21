be2349e307cd375c92d9c2650fd55aa19730383a
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-21 17:03:47 -0400

    much better, now to fix bottom CTAs on home next...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5c7eae82580e12a02928cc4eb36872182decdeb660501f05609f75cd237f77da
-size 5399
+oid sha256:728400e23f999a244edf79848c4f5daa4cfdb121619a7b6d0efad4da01c99991
+size 5342
