641bef76d752da44816be8136a436b81a2d6b0c0
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-21 09:29:43 -0400

    fixed small styling issue

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:01ae898286a9f522cf0b9336da41cfca159c592fbb1c1af32fcebf6332867584
-size 5424
+oid sha256:c03e7d4c64eefa5c63175cc2f7a1ce101efa6535d6816668a2594024d9590a07
+size 6383
