2eae553459836abf584b28779b9d8553b5fb70ef
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-21 09:29:43 -0400

    fixed small styling issue

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -24,85 +24,89 @@ const Copyright = () => (
           textAlign: "center",
           marginTop: "1.5rem",
           fontSize: "0.9rem",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "0.5rem",
         }}
       >
         <Link href="/mission" legacyBehavior>
           <a
             style={{
               color: "#666",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Our Mission
           </a>
         </Link>
-        |
+        <span style={{ color: "#666" }}>|</span>
         <Link href="/statement-of-faith" legacyBehavior>
           <a
             style={{
               color: "#666",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Statement of Faith
           </a>
         </Link>
-        |
+        <span style={{ color: "#666" }}>|</span>
         <Link href="/anti-semitism" legacyBehavior>
           <a
             style={{
               color: "#666",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Statement on Antisemitism
           </a>
         </Link>
-        |
+        <span style={{ color: "#666" }}>|</span>
         <Link href="/blog/counting-the-cost" legacyBehavior>
           <a
             style={{
               color: "#666",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Counting the Cost
           </a>
         </Link>
-        |
+        <span style={{ color: "#666" }}>|</span>
         <Link href="/blog/who-is-yeshua" legacyBehavior>
           <a
             style={{
               color: "#666",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Who is Yeshua?
           </a>
         </Link>
-        |
+        <span style={{ color: "#666" }}>|</span>
         <Link href="/blog/from-the-trenches-an-evangelism-guide" legacyBehavior>
           <a
             style={{
               color: "#666",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             From the Trenches
           </a>
         </Link>
-        |
+        <span style={{ color: "#666" }}>|</span>
         <Link href="/blog/yeshua-and-the-shema" legacyBehavior>
           <a
             style={{
               color: "#666",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
@@ -110,61 +114,77 @@ const Copyright = () => (
           </a>
         </Link>
       </div>
-      <div style={{ fontSize: "0.9rem", marginTop: "0.5rem", color: "#999" }}>
+      <div
+        style={{
+          fontSize: "0.9rem",
+          marginTop: "0.5rem",
+          color: "#999",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "0.5rem",
+        }}
+      >
         <Link href="/" legacyBehavior>
           <a
             style={{
               color: "#888",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Home
           </a>
         </Link>
-        |
-        <Link href="https://chat.humbleberger.org/en?translation=csb" legacyBehavior>
+        <span style={{ color: "#888" }}>|</span>
+        <Link
+          href="https://chat.humbleberger.org/en?translation=csb"
+          legacyBehavior
+        >
           <a
             style={{
               color: "#888",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Chat
           </a>
         </Link>
-        |
+        <span style={{ color: "#888" }}>|</span>
         <Link href="/blog" legacyBehavior>
           <a
             style={{
               color: "#888",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Blog
           </a>
         </Link>
-        |
-        
+        <span style={{ color: "#888" }}>|</span>
+
         <Link href="https://eepurl.com/ja6zNY" legacyBehavior>
           <a
             style={{
               color: "#888",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Subscribe
           </a>
         </Link>
-        |
-        <Link href="https://donorbox.org/humbleberger-ministries" legacyBehavior>
+        <span style={{ color: "#888" }}>|</span>
+        <Link
+          href="https://donorbox.org/humbleberger-ministries"
+          legacyBehavior
+        >
           <a
             style={{
               color: "#888",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
@@ -172,36 +192,46 @@ const Copyright = () => (
           </a>
         </Link>
       </div>
-      <div style={{ fontSize: "0.8rem", marginTop: "0.5rem", color: "#888" }}>
+      <div
+        style={{
+          fontSize: "0.8rem",
+          marginTop: "0.5rem",
+          color: "#888",
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          gap: "0.5rem",
+        }}
+      >
         <Link href="/privacy-policy" legacyBehavior>
           <a
             style={{
               color: "#888",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Privacy Policy
           </a>
         </Link>
-        |
+        <span style={{ color: "#888" }}>|</span>
         <Link href="/terms-of-use" legacyBehavior>
           <a
             style={{
               color: "#888",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
             Terms of Use
           </a>
         </Link>
-        |
+        <span style={{ color: "#888" }}>|</span>
         <Link href="/copyright" legacyBehavior>
           <a
             style={{
               color: "#888",
-              margin: "0 10px",
+              margin: "0 5px",
               textDecoration: "underline",
             }}
           >
