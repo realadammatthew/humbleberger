08cee28a339cb445a080e4d557862e5d0286a597
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-21 09:20:27 -0400

    fixed the favicon

--- src/components/header.js
+++ src/components/header.js
@@ -106,8 +106,11 @@ const Header = () => (
       content="© 2025 Humbleberger Ministries. All rights reserved."
     />
     <link rel="alternate" hrefLang="en" href="https://humbleberger.org/" />
-    <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    <link rel="manifest" href="/site.webmanifest" />
     <link rel="stylesheet" href="/styles.css" />
     <script
       async
