36c6587f5d6ee2e6cd2a5a7eeb707fca0b776718
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-22 23:25:47 -0400

    trying to fix meta tags

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aea193c5fc0717d432b1ea25c17f9064f2ebcc12bd070567207d3980c1e60d61
-size 5145
+oid sha256:23c889b0ae81cdf2cb00a6fdc3f8eb2c9202471c3a29f99a1beb08bc2918ebf5
+size 5531
--- src/i18n.js
+++ src/i18n.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:29bcdd264fca8f60bd19998947f3e9111f5665601d60d6ae4e139c1718c37f00
-size 92405
+oid sha256:5e8fb1cc947edee0e2c75646c021ec929d17f348c2925d8fbabcc88c0e84421a
+size 92491
