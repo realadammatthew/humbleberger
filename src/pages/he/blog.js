86cbc645bef49e137dd9c93aba6c71a207c1d15a
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-23 00:51:42 -0400

    fixed the last major styling issue i could find

--- src/pages/he/blog.js
+++ src/pages/he/blog.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a00e7795d55388c172981fc86854a97adbefacf41c5e539d0dee266f4ac46c81
-size 6092
+oid sha256:650caf3b8e7c1d5bc81c68924eb09088c633e957c96f6b4520b6debcb2652747
+size 6111
