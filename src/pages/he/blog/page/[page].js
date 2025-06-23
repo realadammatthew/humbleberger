05ca2c16443001b759b3a70e6af86b3a93097a94
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-23 00:54:48 -0400

    should fix the remaining major UX problems

--- src/pages/he/blog/page/[page].js
+++ src/pages/he/blog/page/[page].js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c00d1e4c522401942145354711ca45f7bfc27558955c4fcb2bf3d2f8b0ff1eae
-size 7067
+oid sha256:6ebc7c5722fedc0b1a120d7b0961a2b2997ce65b2fe4b689b130668fec2353ff
+size 7086
