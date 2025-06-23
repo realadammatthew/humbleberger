66e574142a4b850f17daaec80688285eb8ab04e4
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-23 00:13:46 -0400

    new ivrit donation link

--- src/i18n.js
+++ src/i18n.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ab5467373261c0b04da92cec01dcfb1db689d83617cc9f23dc7527089f9fa4af
-size 92619
+oid sha256:73bd1da424aff725b76feee31f138e0babafd0afbf67b444b5a8d701c9152aff
+size 92622
