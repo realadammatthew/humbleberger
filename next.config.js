e539dd311f7b0b195e9bdc8857894cc12a14a407
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-06-23 00:27:41 -0400

    let's see if the new headers help

--- next.config.js
+++ next.config.js
@@ -5,4 +5,21 @@ module.exports = {
     unoptimized: true,
   },
   trailingSlash: true,
+  async headers() {
+    return [
+      {
+        source: '/assets/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self'",
+          },
+        ],
+      },
+    ];
+  },
 };
