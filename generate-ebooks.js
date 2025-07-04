7312293c8b9a7ad8ebf3040e84734d9509122276
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-07-03 23:04:56 -0400

    fixed typo as well

--- generate-ebooks.js
+++ generate-ebooks.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6406b6e47a61653f45a7058e710851f4b891cd650debd4dc107fa34d8370ead6
-size 25226
+oid sha256:45b6272faa122a116a63a4279bff7c49c3486b3d62152e5b406392f1baaafc95
+size 25232
