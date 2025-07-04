300742b927fb270d33df99bcd1bca70ba21952ce
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-07-03 21:50:30 -0400

    added pdfs to sitemap

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b4c7eb0ccc830ea0c53e7917afd3b21bdf577203cc20c102aec404a6e89efef5
-size 3634
+oid sha256:71c2b87ecfef7433d60f27caa04b588f4a6df60c9be980f09f8dc17b1f245e52
+size 3959
