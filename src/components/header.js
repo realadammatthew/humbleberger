11a837976252ed8568c4abad35733071716d1369
Adam Matthew Steinberger <adam@matthewsteinberger.com>
2025-07-15 18:12:50 -0400

    added google tag for google ads

--- src/components/header.js
+++ src/components/header.js
@@ -213,6 +213,14 @@ const Header = () => {
           __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-WDGNT0XVNN');`,
         }}
       />
+
+      {/* Google Tag Manager */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17357833341"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'AW-17357833341');`,
+        }}
+      />
     </Head>
   );
 };
